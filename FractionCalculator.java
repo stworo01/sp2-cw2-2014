12bcb995782880c2de0d5f3ed78585c9d480ef05
stworo01 <stefan_1@st-mAcBooKpRo.local>
2014-11-09 21:04:30 +0000

    Now working again
    its not pretty.
    still work todo.

--- FractionCalculator.java
+++ FractionCalculator.java
@@ -1,4 +1,5 @@
 package stworo01;
+
 import java.util.Scanner;
 
 /**
@@ -6,37 +7,43 @@ import java.util.Scanner;
  *
  */
 public class FractionCalculator {
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		//Open FractionCalculator
+		// Open FractionCalculator
 		FractionCalculator calc = new FractionCalculator();
 		// display welcome and instructions
 		String welcome = "Hello and wellcome to the Fraction calculator";
-		String inputDataString = "Enter expression: ";	
+		String inputDataString = "Enter expression: ";
 		System.out.println();
 		System.out.println(welcome);
-		Fraction initialValue = new Fraction(0,1);
+		Fraction initialValue = new Fraction(0, 1);
 		System.out.println(inputDataString);
 		String in = sc.nextLine();
 		Fraction result = calc.evaluate(initialValue, in);
 		String out = result.toString();
-		System.out.println("The answer is: "+out);		
-		sc.close();	
+		System.out.println("The answer is: " + out);
+		sc.close();
 	}
+
 	/**
 	 * A method that checks if the first character is a digit
+	 * 
 	 * @param str
-	 * @return
+	 *            string to be checked
+	 * @return true if digit is present otherwise false
 	 */
 	public boolean isNum(String str) {
-		char c =str.charAt(0);
-		if (Character.isDigit(c)) {return true;}
+		char c = str.charAt(0);
+		if (Character.isDigit(c)) {
+			return true;
+		}
 		return false;
 	}
+
 	/**
 	 * Method that takes a fraction object and a user input string and carries
 	 * out calculations based on the operators and numbers contained in the
@@ -46,53 +53,51 @@ public class FractionCalculator {
 	 * @param input
 	 * @return
 	 */
-	
+
 	public Fraction evaluate(Fraction frac, String input) {
 		int numerator = 0;
 		int denominator = 1;
-		
+
 		// split string
 		String delimiter = "[ /]";
-		String [] inputArray = input.split(delimiter);
-		// first fraction 
-		Fraction stored = new Fraction (numerator,denominator);
+		String[] inputArray = input.split(delimiter);
+		// first fraction
+		Fraction stored = new Fraction(numerator, denominator);
 
-		for (int i = 0; i < inputArray.length - 1; i++) {
-			
-			if(isNum(inputArray[i])) {
+		for (int i = 1; i < inputArray.length; i++) {
+			if (isNum(inputArray[i])) {
 				int numerator1 = Integer.parseInt(inputArray[0]);
 				int denominator1 = Integer.parseInt(inputArray[1]);
 				stored = new Fraction(numerator1, denominator1);
-				
 			}
-			//addition
-			if(inputArray[i].equals("+")) {
+			// addition
+			if (inputArray[i].equals("+")) {
 				int num = Integer.parseInt(inputArray[i + 1]);
 				int denm = Integer.parseInt(inputArray[i + 2]);
-				System.out.println("i'm here");
-				System.out.println(stored.toString());//test
-				Fraction fracTwo = new Fraction(num,denm);
+				Fraction fracTwo = new Fraction(num, denm);
 				stored = stored.add(fracTwo);
-				System.out.println(stored.toString());//test
+				i = i + 2;
 			}
-			//multiply
-			if(inputArray[i].equals("*")) {
+			// multiply
+			if (inputArray[i].equals("*")) {
 				int num = Integer.parseInt(inputArray[i + 1]);
 				int denm = Integer.parseInt(inputArray[i + 2]);
-				Fraction fracTwo = new Fraction(num,denm);
-				stored = stored.multiply(fracTwo);	
+				Fraction fracTwo = new Fraction(num, denm);
+				stored = stored.multiply(fracTwo);
+				i = i + 2;
 			}
-			//subtract
-			if(inputArray[i].equals("-")) {
+			// subtract
+			if (inputArray[i].equals("-")) {
 				int num = Integer.parseInt(inputArray[i + 1]);
 				int denm = Integer.parseInt(inputArray[i + 2]);
-				Fraction fracTwo = new Fraction(num,denm);
+				Fraction fracTwo = new Fraction(num, denm);
 				stored = stored.sub(fracTwo);
-				;
+				i = i + 2;
+
 			}
 		}
-		System.out.println(stored.toString());//test
+
 		return stored;
 	}
-	
+
 }
